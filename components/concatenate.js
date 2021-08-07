87fb5d1e509fba06f566121effb4bc66939adf33
Ryan Ashiruma <ryananyangu@gmail.com>
2021-08-07 13:41:10 +0300

    complete uploder to cloudinary

--- components/concatenate.js
+++ components/concatenate.js
@@ -1,136 +1,129 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-function Concatenate () {
+function Concatenate() {
+  let urls = [];
 
-    const handleUpload = async (e) => {
-        
-        function readAsDataURL(file){
-            return new Promise(function(resolve,reject){
-                let fr = new FileReader();
-    
-                fr.onload = function(){
-                    resolve(fr.result);
-                };
-    
-                fr.onerror = function(){
-                    reject(fr);
-                };
-    
-                fr.readAsDataURL(file);
-            });
-        }
-        // Handle multiple fileuploads
-        let files = e.target.files;
-        console.log("files", files)
-       
-        let readers = [];
+  const handleUpload = async (e) => {
+    // Handle multiple fileuploads
+    let files = e.target.files;
+    console.log("files", files);
 
-        // Abort if there were no files selected
-        if(!files.length) return;
-        
-        // Store promises in array
-        for(let i = 0;i < files.length;i++){
-            console.log("for_counter", files[i])
-            readers.push(readAsDataURL(files[i]));
-        }
-        // Trigger Promises
-        Promise.all(readers).then((values) => {
-            // Values will be an array that contains an item
-            // with the text of every selected file
-            // ["File1 Content", "File2 Content" ... "FileN Content"]
-            console.log("base64_array",values);
-            uploadVideos(values)
-        });
-        const uploadVideos = async (base64url) => {
-            console.log("base64_to_POST", base64url)
-            try {
-                await fetch('/api/upload', {
-                    method: 'POST',
-                    body: JSON.stringify({ data:base64url}),
-                    headers: { 'Content-Type': 'application/json' },
-                    })
-                    .then(res => res.json())
-                    
-            } catch (error) {
-              console.error(error);
-            }
-        }
+    // Abort if there were no files selected
+    if (!files.length) return;
 
+    // Store promises in array
+    for (let file of files) {
+      await readFile(file).then((encoded_file) => {
+        uploadVideos(encoded_file,file.name);
+      });
     }
+  };
 
-    const Change = async (e) => {
-        const videoElement = document.getElementById("vida");
-        const vidClips = [
-            'https://res.cloudinary.com/dogjmmett/video/upload/v1628064580/STB_Stuttgart_F%C3%B6hrich_U6_Line_Entering_Station_VIDEO.webm.160p_kfpkgc.webm',
-            'https://demo-res.cloudinary.com/video/upload/w_500/dog.webm',
-            'https://res.cloudinary.com/dogjmmett/video/upload/v1628064576/rabbit320_hyftw3.webm'
-        ];
-        console.log("videoClips", vidClips);
- 
-        // Get video clips as buffers
-        const clipsToAppend = await Promise.all(
-            vidClips.map(async (vidUrl) => {
-                return (await fetch(vidUrl)).arrayBuffer();
-            })
-        );
-        console.log('clips to appended', clipsToAppend)  
-        
-        // Normal setup, with MediaSource, Object URL, and prepped SourceBuffer
-        const mediaSource = new MediaSource();
-        videoElement.src = URL.createObjectURL(mediaSource);
+  function readFile(file) {
+    return new Promise(function (resolve, reject) {
+      let fr = new FileReader();
 
-        // mode = sequence
-    const sourceBuffer = await new Promise((resolve, reject) => {
-            
-        const getSourceBuffer = () => {
-            
-            try {
-                let mimeCodec = 'video/webm; codecs="vp8,vorbis"';
-                const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec)
-                resolve(sourceBuffer);
-            } catch (e) {
-                reject(e);
-            }
-        };
+      fr.onload = function () {
+        resolve(fr.result);
+      };
 
-            if (mediaSource.readyState === 'open') {
-                getSourceBuffer();
-            } else {
-                mediaSource.addEventListener('sourceopen', getSourceBuffer);
-            }
+      fr.onerror = function () {
+        reject(fr);
+      };
+
+      fr.readAsDataURL(file);
+    });
+  }
+
+  const uploadVideos = (base64url,name) => {
+    console.log("base64_to_POST");
+    try {
+      fetch("/api/upload", {
+        method: "POST",
+        body: JSON.stringify({ data: base64url }),
+        headers: { "Content-Type": "application/json" },
+      }).then((response) => {
+        console.log(name,response.status);
+        response.json().then((data) => {
+          urls.push(data.data);
         });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        /**
-	 * Pointer to last vid appended out of source list
-	 */
-	let clipIndex = 0;
-	sourceBuffer.onupdateend = () => {
-		if (clipIndex < vidClips.length - 1) {
-			clipIndex++;
-			sourceBuffer.appendBuffer(clipsToAppend[clipIndex]);
-		} else {
-			// Done!
-			mediaSource.endOfStream();
-			videoElement.play();
-		}
-	};
+  const Change = async (e) => {
+    const videoElement = document.getElementById("vida");
+    const vidClips = urls;
+    console.log("videoClips", vidClips);
+
+    // Get video clips as buffers
+    const clipsToAppend = await Promise.all(
+      vidClips.map(async (vidUrl) => {
+        return (await fetch(vidUrl)).arrayBuffer();
+      })
+    );
+    console.log("clips to appended", clipsToAppend);
+
+    // Normal setup, with MediaSource, Object URL, and prepped SourceBuffer
+    const mediaSource = new MediaSource();
+    videoElement.src = URL.createObjectURL(mediaSource);
+
+    // mode = sequence
+    const sourceBuffer = await new Promise((resolve, reject) => {
+      const getSourceBuffer = () => {
+        try {
+          let mimeCodec = 'video/webm; codecs="vp8,vorbis"';
+          const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+          resolve(sourceBuffer);
+        } catch (e) {
+          reject(e);
+        }
+      };
+
+      if (mediaSource.readyState === "open") {
+        getSourceBuffer();
+      } else {
+        mediaSource.addEventListener("sourceopen", getSourceBuffer);
+      }
+    });
+
+    /**
+     * Pointer to last vid appended out of source list
+     */
+    let clipIndex = 0;
+    sourceBuffer.onupdateend = () => {
+      if (clipIndex < vidClips.length - 1) {
+        clipIndex++;
+        sourceBuffer.appendBuffer(clipsToAppend[clipIndex]);
+      } else {
+        // Done!
+        mediaSource.endOfStream();
+        videoElement.play();
+      }
+    };
 
     // This will kick off event listener chain above
-	sourceBuffer.appendBuffer(clipsToAppend[clipIndex]);
+    sourceBuffer.appendBuffer(clipsToAppend[clipIndex]);
 
-	// Debug Info
-	 
-    console.log("sourceBuffer",sourceBuffer)
-    console.log("mediaSource", mediaSource)
-    console.log("videoElement", videoElement)
+    // Debug Info
 
-    }
-    return(
-        <div>
-            <video width="300"  controls id="vida" /><br/><br/>
-            <Button variant="contained" color ="primary" onClick={Change}>Concatenate</Button>
-            <input type="file" onChange= {handleUpload} multiple/>
-        </div>
-    )
-}export default Concatenate
+    console.log("sourceBuffer", sourceBuffer);
+    console.log("mediaSource", mediaSource);
+    console.log("videoElement", videoElement);
+  };
+  return (
+    <div>
+      <video width="300" controls id="vida" />
+      <br />
+      <br />
+      <Button variant="contained" color="primary" onClick={Change}>
+        Concatenate
+      </Button>
+      <input type="file" onChange={handleUpload} multiple />
+    </div>
+  );
+}
+export default Concatenate;
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,41 +1,43 @@
-var cloudinary = require('cloudinary').v2
-var createArray = require('create-array');
+var cloudinary = require("cloudinary").v2;
+var createArray = require("create-array");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
 });
 export const config = {
-    api: {
-      bodyParser: {
-        sizeLimit: '20mb',
-      },
+  api: {
+    bodyParser: {
+      sizeLimit: "20mb",
     },
-}
+  },
+};
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
+  if (req.method === "POST") {
     // Process a POST request
-      console.log("api works start here")
-      try {
-        let fileStr = req.body.data;
-        console.log("backend received", fileStr.length, "files")
-        for(let i=0; i<fileStr.length;i++) 
-        {
-          // console.log("received data",[i])
-          const uploadedResponse = await cloudinary.uploader.
-                upload_large(fileStr[i],{ 
-                resource_type: "video",
-                chunk_size: 6000000,
-                }) 
-                 
-        } 
-           
-      } catch (error) {
-        console.log("error",error)
-        // res.status(500).json(error,'Something wrong')
-      }
+    let uploaded_url = ""
+    try {
+      let fileStr = req.body.data;
+      console.log("backend received");
+
+        const uploadedResponse = await cloudinary.uploader.upload_large(
+          fileStr,
+          {
+            resource_type: "video",
+            chunk_size: 6000000,
+          }
+        );
+        console.log(uploaded_url)
+        uploaded_url = uploadedResponse.secure_url
+      
+    } catch (error) {
+      console.log("error", error);
+      res.status(500).json({ error: "Something wrong" });
     }
+    
+    res.status(200).json({ data : uploaded_url });
+  }
 }
